f680f2279db598f43feb2573c171d9f21b92b800
Beemo <ksu3101@coupang.com>
2017-03-01 12:02:07 +0900

    update contents

--- app/src/main/java/com/example/mvvm/mvvmexample/common/base/BaseActivity.java
+++ app/src/main/java/com/example/mvvm/mvvmexample/common/base/BaseActivity.java
@@ -1,10 +1,27 @@
 package com.example.mvvm.mvvmexample.common.base;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+
+import com.tsengvn.typekit.TypekitContextWrapper;
 
 /**
  * @author KangSungWoo
  * @since 2017-02-27
  */
 public class BaseActivity extends AppCompatActivity {
+
+	@Override
+	protected void attachBaseContext(Context newBase) {
+		super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+			getSupportFragmentManager().popBackStack();
+			return;
+		}
+		super.onBackPressed();
+	}
 }
